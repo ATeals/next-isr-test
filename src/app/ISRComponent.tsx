7a23ee7a2b187f6c265629bb7a949fe67c761d49
ateals <ateals@icloud.com>
2024-01-09 00:04:57 +0900

    domain

--- src/app/ISRComponent.tsx
+++ src/app/ISRComponent.tsx
@@ -1,4 +1,7 @@
-export const DOMAIN = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+export const DOMAIN =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://next-isr-test-iota.vercel.app";
 
 export const ISRComponent = async () => {
   const res = await fetch(`${DOMAIN}/api/random`, {
